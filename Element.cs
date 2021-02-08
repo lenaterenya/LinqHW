40c537219682aa9fa87209ccf86c89a0454a2a3f
Lena <tinl88@mail.ru>
2021-02-08 15:20:51 +0300

    deleted reduntant

--- Element.cs
+++ Element.cs
@@ -18,11 +18,7 @@ namespace LinqHW
         public override int GetHashCode()
         {
 
-            var one = Length.GetHashCode();
-            var two = Width.GetHashCode();
-            var three = GetType().GetHashCode();
-
-            return Length.GetHashCode() + Width.GetHashCode() + LeftUpperLocation.X.GetHashCode() + LeftUpperLocation.Y.GetHashCode()
+          return Length.GetHashCode() + Width.GetHashCode() + LeftUpperLocation.X.GetHashCode() + LeftUpperLocation.Y.GetHashCode()
                    + GetType().GetHashCode();
         }
         public Element(Point location, int width, int length)
